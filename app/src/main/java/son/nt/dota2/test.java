695b7567e9f143c5e4f014206ecd7df779c604f6
Son.Nguyen <thanhsontube@gmail.com>
2015-07-06 13:18:41 +0700

    COnfig product Flaviour

---
+++ app/src/main/java/son/nt/dota2/test.java
@@ -0,0 +1,12 @@
+package son.nt.dota2;
+
+import android.content.Context;
+
+/**
+ * Created by Sonnt on 7/6/15.
+ */
+public class Test {
+    public void test1(Context context) {
+        context.getString(R.string.app_name);
+    }
+}
