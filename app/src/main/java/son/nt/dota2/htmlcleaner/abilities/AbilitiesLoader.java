af0ca0b3c60ffac87e4fe74df366baefc2e79247
Son.Nguyen <thanhsontube@gmail.com>
2015-07-17 10:32:44 +0700

    remove pre abilities

--- app/src/main/java/son/nt/dota2/htmlcleaner/abilities/AbilitiesLoader.java
+++ app/src/main/java/son/nt/dota2/htmlcleaner/abilities/AbilitiesLoader.java
@@ -50,6 +50,7 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
             data = tagNode.evaluateXPath(xpath);
 
             List<AbilityDto> listAbilities = new ArrayList<>();
+            listAbilities.clear();
             if (data != null && data.length > 0) {
                 AbilityDto dto;
 
@@ -68,11 +69,9 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
                             //get skill voice
                             String tnPath = "//span[@style='position:relative; top:-2px;']";
                             Object[] oTn = nodeA.evaluateXPath(tnPath);
-                            Logger.debug(TAG, ">>>" + "oTN:" + oTn.length);
                             if (oTn.length > 0) {
                                 TagNode tVoice = (TagNode) oTn[0];
                                 String voice = tVoice.getChildTagList().get(0).getAttributeByName("href");
-                                Logger.debug(TAG, ">>>" + "Voice:" + voice);
                                 dto.sound = voice;
                             }
 
@@ -80,56 +79,45 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
                             Logger.error(TAG, ">>>" + "ERROR VOICE:" + e.toString());
                             e.printStackTrace();
                         }
-
-                        int sizeB = nodeA.getChildTagList().size();
-
-                        Logger.debug(TAG, ">>>" + "B:" + sizeB);
-
                         // skill name
                         TagNode node1 = nodeA.getChildTagList().get(0);
                         String skillName = node1.getText().toString().substring(1, node1.getText().toString().indexOf("   ")).trim();
-                        Logger.debug(TAG, ">>>" + "skill name:" + skillName);
                         dto.name = skillName;
 
 
+                        //Skill Image
+
                         TagNode node2 = nodeA.getChildTagList().get(1);
-                        Logger.debug(TAG, ">>>" + "Node2 text:" + node2.getText() + ";size:" + node2.getChildTagList().size());
                         String imgPath = "//img[@src]";
                         Object[] oImgs = node2.evaluateXPath(imgPath);
-                        Logger.debug(TAG, ">>>" + "oImgs:" + oImgs.length);
                         if (oImgs.length > 0) {
                             TagNode tnI = (TagNode) oImgs[0];
-                            Logger.debug(TAG, ">>>" + "tnI:" + tnI.getAttributeByName("src"));
                             dto.linkImage = tnI.getAttributeByName("src");
+                            Logger.debug(TAG, ">>>" + "Image:" + dto.linkImage);
                         }
                         //skill type
 
                         String affects = node2.getText().toString();
                         String[] arr = affects.split("\n");
-                        List<String> listSkillType = new ArrayList<>();
-                        Logger.debug(TAG, ">>>" + "arr:" + arr.length);
-//                        int ii = 0;
+                        List<String> skillTypes = new ArrayList<>();
 
                         for (String s : arr) {
-//                            Logger.debug(TAG, ">>>" + "arr:" + s + ":" + ii++);
                             if (!TextUtils.isEmpty(s) && !s.equals(" ")) {
-                                listSkillType.add(s);
+                                skillTypes.add(s);
                             }
                         }
-                        Logger.debug(TAG, ">>>" + "ListSkill:" + listSkillType.size());
-
                         String ability = null;
 
                         String affect = null;
                         String damage = null;
-                        if (listSkillType.size() == 6) {
-                            ability = listSkillType.get(3);
-                            affect = listSkillType.get(4);
-                            damage = listSkillType.get(5);
+                        if (skillTypes.size() == 6) {
+                            ability = skillTypes.get(3);
+                            affect = skillTypes.get(4);
+                            damage = skillTypes.get(5);
                         }
-                        if (listSkillType.size() == 4) {
-                            ability = listSkillType.get(2);
-                            affect = listSkillType.get(3);
+                        if (skillTypes.size() == 4) {
+                            ability = skillTypes.get(2);
+                            affect = skillTypes.get(3);
                         }
 
                         dto.setTypes(ability, affect, damage);
@@ -146,7 +134,6 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
 
                         String xPathTable = "//table";
                         Object[] oTable = nodeTable.evaluateXPath(xPathTable);
-                        Logger.debug(TAG, ">>>" + "Table:" + oTable.length);
                         if (oTable.length > 0) {
                             //cooldown and mana
                             String xPathMana = "//a[@title='Mana']";
@@ -155,7 +142,6 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
                             if (oMana.length > 0) {
                                 String xPathCoMna = "./tbody/tr/td";
                                 Object[] oComa = nodeCoolDownMana.evaluateXPath(xPathCoMna);
-                                Logger.debug(TAG, ">>>" + "oComa:" + oComa.length);
 
 
                                 //get cooldown
@@ -164,7 +150,6 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
 
                                 String coolDowns = nodeCoolDown.getText().toString().replace(".", "").trim();
                                 String[] arrCoolDown = coolDowns.split("/");
-                                Logger.debug(TAG, ">>>" + "arrCoolDown:" + arrCoolDown.length);
                                 dto.coolDowns.clear();
                                 if (arrCoolDown.length == 1) {
 
@@ -212,53 +197,19 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
                             }
 
                         }
-
-
-                        sizeB = 0;
-                        for (int j = 0; j < sizeB; j++) {
-                            try {
-                                Logger.debug(TAG, ">>>" + "-------BBBB-------:" + sizeB + ":" + j);
-                                TagNode nodeB = nodeA.getChildTagList().get(j);
-                                int sizeC = nodeB.getChildTagList().size();
-                                for (int k = 0; k < sizeC; k++) {
-                                    try {
-                                        TagNode nodeC = nodeB.getChildTagList().get(k);
-                                        Logger.debug(TAG, ">>>" + "--CCC---:" + sizeC + ":" + k + ";nodeC:" + nodeC.getChildTagList().size());
-                                        if (k == 0 && j == 0) {
-                                            skillName = nodeC.getText().toString().substring(1, nodeC.getText().toString().indexOf("   "));
-                                            Logger.debug(TAG, ">>>" + "skill name:" + skillName);
-                                            dto.name = skillName;
-                                        }
-                                        Logger.debug(TAG, ">>>" + "tn2:" + nodeC.getText());
-                                    } catch (Exception e) {
-                                        Logger.error(TAG, ">>>" + "ERROR CCCC:" + e.toString());
-                                        e.printStackTrace();
-                                    }
-                                }
-                            } catch (Exception e) {
-                                Logger.error(TAG, ">>>" + "ERROR BBBB:" + e.toString());
-                                e.printStackTrace();
+                        boolean isAdd = true;
+                        for (AbilityDto d : listAbilities) {
+                            String old = d.name.toLowerCase().replace(" ", "").trim();
+                            String new1 = dto.name.toLowerCase().replace(" ", "").trim();
+                            if (new1.contains(old) || old.contains(new1) || new1.contains("(pre")
+                                    || (dto.linkImage != null && dto.linkImage.contains("Unknown_icon.png"))) {
+                                isAdd = false;
+                                break;
                             }
                         }
-                        if (listAbilities.size() > 0) {
 
-                            for (AbilityDto d : listAbilities) {
-                                String old = d.name.toLowerCase().replace(" ", "").trim();
-                                String new1 = dto.name.toLowerCase().replace(" ", "").trim();
-                                if (new1.contains(old) || old.contains(new1)) {
-
-                                } else {
-                                    listAbilities.add(dto);
-
-                                }
-                            }
-                        } else {
+                        if (isAdd) {
                             listAbilities.add(dto);
-                        }
-
-                        //Stop add another skill
-                        if (dto.isUltimate) {
-                            break;
                         }
 
                     } catch (Exception e) {
@@ -276,96 +227,6 @@ public abstract class AbilitiesLoader extends ContentLoader<List<AbilityDto>> {
         }
         return null;
     }
-
-//    @Override
-//    protected List<AbilityDto> handleStream(InputStream in) throws IOException {
-//        try {
-//            HtmlCleaner cleaner = new HtmlCleaner();
-//            CleanerProperties props = cleaner.getProperties();
-//            props.setAllowHtmlInsideAttributes(true);
-//            props.setAllowMultiWordAttributes(true);
-//            props.setRecognizeUnicodeChars(true);
-//            props.setOmitComments(true);
-//            Object[] data = null;
-//
-//            TagNode tagNode = cleaner.clean(in);
-//            String xpath = "//table[@class='abilitytable']";
-//            data = tagNode.evaluateXPath(xpath);
-//
-//            List<AbilityDto> listAbilities = new ArrayList<>();
-//            if (data != null && data.length > 0) {
-//                AbilityDto dto;
-//
-//                int sizeA = data.length;
-//
-//                for (int i = 0; i < sizeA; i++) {
-//
-//
-//                    try {
-//                        Logger.debug(TAG, ">>>" + "==========AAAA==========:" + sizeA + ":" + i);
-//                        dto = new AbilityDto();
-//
-//                        //TODO need be care full with this
-//                        TagNode nodeA = ((TagNode) data[i]).getChildTagList().get(0);
-//
-//                        try {
-//                            //get skill voice
-//                            String tnPath = "//span[@style='position:relative; top:-2px;']";
-//                            Object[] oTn = nodeA.evaluateXPath(tnPath);
-//                            Logger.debug(TAG, ">>>" + "oTN:" + oTn.length);
-//
-//                            TagNode tVoice = (TagNode) oTn[0];
-//                            String voice = tVoice.getChildTagList().get(0).getAttributeByName("href");
-//                            Logger.debug(TAG, ">>>" + "Voice:" + voice);
-//
-//                            dto.sound = voice;
-//                        } catch (XPatherException e) {
-//                            Logger.error(TAG, ">>>" + "ERROR VOICE:" + e.toString());
-//                            e.printStackTrace();
-//                        }
-//
-//                        int sizeB = nodeA.getChildTagList().size();
-//                        for (int j = 0; j < sizeB; j++) {
-//                            try {
-//                                Logger.debug(TAG, ">>>" + "-------BBBB-------:" + sizeB + ":" + j);
-//                                TagNode nodeB = nodeA.getChildTagList().get(j);
-//                                int sizeC = nodeB.getChildTagList().size();
-//                                for (int k = 0; k < sizeC; k++) {
-//                                    try {
-//                                        TagNode nodeC = nodeB.getChildTagList().get(k);
-//                                        Logger.debug(TAG, ">>>" + "--CCC---:" + sizeC + ":" + k + ";nodeC:" + nodeC.getChildTagList().size());
-//                                        if (k == 0 && j ==0) {
-//                                            String skillName = nodeC.getText().toString().substring(1, nodeC.getText().toString().indexOf("   "));
-//                                            Logger.debug(TAG, ">>>" + "skill name:" + skillName);
-//                                            dto.name = skillName;
-//                                        }
-//                                        Logger.debug(TAG, ">>>" + "tn2:" + nodeC.getText());
-//                                    } catch (Exception e) {
-//                                        Logger.error(TAG, ">>>" + "ERROR CCCC:" + e.toString());
-//                                        e.printStackTrace();
-//                                    }
-//                                }
-//                            } catch (Exception e) {
-//                                Logger.error(TAG, ">>>" + "ERROR BBBB:" + e.toString());
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                        listAbilities.add(dto);
-//                    } catch (Exception e) {
-//                        Logger.error(TAG, ">>>" + "ERROR AAAA:" + e.toString());
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//            return listAbilities;
-//
-//
-//        } catch (Exception e) {
-//            Logger.error(TAG, ">>>" + "error ROOT:" + e.toString());
-//
-//        }
-//        return null;
-//    }
 
     private void upLoadToParseService(final List<AbilityDto> listRoles) {
 
